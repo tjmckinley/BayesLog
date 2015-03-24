b9984d0e68d23b1c4271c0fc45f67588a88c89fc
TJ McKinley <tjm44@cam.ac.uk>
2015-03-24 16:22:54 +0000

    Added window function.

--- analysisfunctions.R
+++ analysisfunctions.R
@@ -243,4 +243,34 @@ summary.varselect <- function(output, topmodels = 5, ...)
         print(indicators1, quote = F)
     }
 }
+
+# window function for 'bayesord' objects
+window.varselect <- function(x, start = NA, end = NA, thin = NA, chains = NA, ...) {
+    y <- x
+    if (!is.na(chains)) {
+        x <- x[[1]][chains]
+    }
+    else x <- x[[1]]
     
+    if(is.na(start)) {
+        if(is.na(end)) {
+            if(!is.na(thin)) x <- window(x, thin = thin)
+        }
+        else {
+            if(!is.na(thin)) x <- window(x, end = end, thin = thin)
+            else x <- window(x, end = end)
+        }
+    }
+    else {
+        if(is.na(end)) {
+            if(!is.na(thin)) x <- window(x, start = start, thin = thin)
+            else x <- window(x, start = start)
+        }
+        else {
+            if(!is.na(thin)) x <- window(x, start = start, end = end, thin = thin)
+            else x <- window(x, start = start, end = end)
+        }
+    }
+    y[[1]] <- x
+    y
+} 
