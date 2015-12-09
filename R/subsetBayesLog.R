a79e42458bcbe86987c1a1911d960112db6b198b
TJ McKinley <t.mckinley@exeter.ac.uk>
2015-12-09 05:10:39 +0000

    Fixed subsetting

--- R/subsetBayesLog.R
+++ R/subsetBayesLog.R
@@ -3,32 +3,35 @@
 #' \code{subset} method for class \code{bayesLog}
 #' 
 #' Extracts a subset from a \code{\link{bayesLog}} object.
-#' Essentially a wrapper for \code{\link[coda]{[.mcmc}} functions from the \code{coda}
-#' package.
+#' Differs slightly from \code{\link[coda]{mcmc.subset}} in that it
+#' always returns a \code{bayesLog} object, but with the iterations
+#' reset (if \code{i} is not missing).
 #' 
 #' @param x a \code{bayesLog} object, usually as a result of a call to
 #' \code{\link{bayesLog}}.
-#' @param \dots additional arguments to be passed to \code{\link[coda]{[.mcmc}}
+#' @param i the iterations to extract
+#' @param j the variables to extract
+#' @param \dots not used here
 #' @author TJ McKinley
-#' @seealso \code{\link{bayesLog}} \code{\link{plot.bayesLog}} \code{\link{window.bayesLog}} \code{\link[coda]{[.mcmc}}
+#' @seealso \code{\link{bayesLog}} \code{\link{plot.bayesLog}} \code{\link{window.bayesLog}}
 #'
 #' @return A \code{bayesLog} object
 #'
 #' @export
 #'
 
-"[.bayesLog" <- function(x, ...)
+"[.bayesLog" <- function(x, i, j)
 {
     stopifnot(class(x) == "bayesLog")
     
     #extract 'mcmc' object
-    y <- x$post
+    y <- as.matrix(x$post)
     
     #extract subset
-    y <- y[...]
+    y <- y[i, j, drop = F]
     
     #return bayesLog object
-    x$post <- y
+    x$post <- as.mcmc(y)
     x
 }
 
