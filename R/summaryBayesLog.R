2df337a38748d77339fad69b63da9cb9eb2ebdbb
TJ McKinley <t.mckinley@exeter.ac.uk>
2015-12-11 20:15:27 +0000

    Amended summary code to subset by regression, random effects or all.

--- R/summaryBayesLog.R
+++ R/summaryBayesLog.R
@@ -8,6 +8,10 @@
 #' 
 #' @param object a \code{bayesLog} object, usually as a result of a call to
 #' \code{\link{bayesLog}}.
+#' @param vars a character specifying whether \code{"all"} parameters are to be plotted,
+#' or just the regression terms (\code{"reg"}), or one of the random effect terms (\code{"rand"}).
+#' If \code{vars = "rand"}, then the user must also specify a \code{rand} argument denoting
+#' which term to extract.
 #' @param \dots additional arguments to be passed to \code{\link[coda]{summary.mcmc}}
 #' @author TJ McKinley
 #' @seealso \code{\link{bayesLog}} \code{\link{plot.bayesLog}} \code{\link{window.bayesLog}} \code{\link[coda]{summary.mcmc}}
@@ -18,15 +22,35 @@
 #'
 #' @aliases print.bayesLog
 
-summary.bayesLog <- function(object, ...)
+summary.bayesLog <- function(object, vars = c("all", "reg", "rand"), rand, ...)
 {
     stopifnot(class(object) == "bayesLog")
     
+    stopifnot(is.character(vars))
+    stopifnot(!is.na(match(vars[1], c("reg", "rand", "all"))))
+    
     #extract 'mcmc' object
-    object <- object$post
+    y <- object$post
+    
+    if(vars[1] == "reg") y <- y[, 1:(object$nregpars + object$nrand)]
+    else
+    {
+        if(vars[1] == "rand")
+        {
+            stopifnot(object$nrand > 0 & !missing(rand))
+            stopifnot(is.numeric(rand) & length(rand) == 1 & abs(floor(rand) - rand) < .Machine$double.eps ^ 0.5)
+            stopifnot(!is.na(match(rand, 1:object$nrand)))
+            
+            z <- rep(1:object$nrand, object$nrandlevels)
+            randnames <- object$randnames[rand]
+            inds <- which(!is.na(match(z, rand)))
+            inds <- inds + (object$nregpars + object$nrand)
+            y <- y[, inds]
+        }
+    }
     
     #summarise
-    summary(object, ...)
+    summary(y, ...)
 }
 
 #generic print function for \code{bayesLog} objects
