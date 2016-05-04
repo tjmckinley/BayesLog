dcb7b467b31e57bf8de09ad80598dc8a7d3ea352
TJ McKinley <t.mckinley@exeter.ac.uk>
2016-05-04 11:45:58 +0100

    Window function can now only subset by iteration number.

--- R/windowBayesLog.R
+++ R/windowBayesLog.R
@@ -7,43 +7,18 @@
 #' 
 #' @param x a \code{bayesLog} x, usually as a result of a call to
 #' \code{\link{bayesLog}}.
-#' @param vars a character specifying whether \code{"all"} parameters are to be plotted,
-#' or just the regression terms (\code{"reg"}), or one of the random effect terms (\code{"rand"}).
-#' If \code{vars = "rand"}, then the user must also specify a \code{rand} argument denoting
-#' which term to extract.
-#' @param rand an integer denoting which random effect to extract.
 #' @param \dots arguments to pass to \code{\link{window.mcmc}}
 #' @author TJ McKinley
 #' @seealso \code{\link{bayesLog}} \code{\link{summary.bayesLog}} \code{\link{window.bayesLog}} \code{\link[coda]{window.mcmc}}
 #'
 #' @export
 
-window.bayesLog <- function(x, vars = c("all", "reg", "rand"), rand, ...)
+window.bayesLog <- function(x, ...)
 {
     stopifnot(class(x) == "bayesLog")
-    stopifnot(is.character(vars))
-    stopifnot(!is.na(match(vars[1], c("reg", "rand", "all"))))
-    
-    if(vars[1] == "rand" & missing(rand)) stop("Need to specify which random effects to extract")
     
     #extract 'mcmc' x
     y <- x$post
-    
-    if(vars[1] == "reg") y <- y[, 1:(x$nregpars + x$nrand)]
-    else
-    {
-        if(vars[1] == "rand")
-        {
-            stopifnot(x$nrand > 0 & !missing(rand))
-            stopifnot(is.numeric(rand) & length(rand) == 1 & abs(floor(rand) - rand) < .Machine$double.eps ^ 0.5)
-            stopifnot(!is.na(match(rand, 1:x$nrand)))
-            
-            z <- rep(1:x$nrand, x$nrandlevels)
-            inds <- which(!is.na(match(z, rand)))
-            inds <- inds + (x$nregpars + x$nrand)
-            y <- y[, inds]
-        }
-    }
     
     #extract subset
     y <- window(y, ...)
